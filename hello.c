caf14f4311c4daef4709cca8a82a582e184f0a75
khuongtra121 <59979295+khuongtra121@users.noreply.github.com>
2020-01-16 15:21:35 -0600

    Update hello.c
    I added my name and Major into Danny Schroeder file

--- hello.c
+++ hello.c
@@ -7,7 +7,9 @@ Date: 2020/01/14
 int main(void) {
 	
 	printf("Danny Schroeder\n");
-	printf("Electrical Engineering\n")
+	printf("Electrical Engineering\n");
+	printf("Khuong Tra\n");
+	printf("Computer Science\n");
 	
 	return 0;
 }
